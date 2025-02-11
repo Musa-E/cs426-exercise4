8aec73a6a812a24eac17d14e0be960660ad438f0
nherr6 <129147627+nherr6@users.noreply.github.com>
2025-02-10 22:08:55 -0600

    Add cooldown to building

--- Assets/Scripts/BuildingScript.cs
+++ Assets/Scripts/BuildingScript.cs
@@ -5,17 +5,37 @@ public class BuildingScript : MonoBehaviour
 {
     public Material activeMat;
     public Material idleMat;
+    bool isActive = true;
+    public float coolDownTime = 10;
+    [SerializeField] private TMP_Text ItemList;
+
     void start()
     {
         GetComponent<Renderer>().material = activeMat;
     }
 
-    [SerializeField] private TMP_Text ItemList;
+    void Update(){
+        if(!isActive && coolDownTime > 0){
+            coolDownTime -= Time.deltaTime;
+            Debug.Log(coolDownTime);
+        }
+        if(!isActive && coolDownTime <= 0)
+        {
+            coolDownTime = 10;
+            GetComponent<Renderer>().material = activeMat;
+            isActive = true;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("hit detected");
-        GetComponent<Renderer>().material = idleMat;
-        ItemList.text += '\n' + this.name;
+        if (isActive){
+            GetComponent<Renderer>().material = idleMat;
+            ItemList.text += '\n' + this.name;
+            
+            isActive = false;
+        }
     }
 
 }
